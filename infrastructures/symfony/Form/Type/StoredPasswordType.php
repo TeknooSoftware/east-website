ca307ce05e81fc89ec4640bfae7b9d43f9d5f0b3
Richard Deloge <richarddeloge@gmail.com>
2021-08-26 13:12:34 +0200

    Refactoring authenticating wip and qa

--- infrastructures/symfony/Form/Type/StoredPasswordType.php
+++ infrastructures/symfony/Form/Type/StoredPasswordType.php
@@ -26,11 +26,8 @@ declare(strict_types=1);
 namespace Teknoo\East\WebsiteBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Teknoo\East\Website\Object\StoredPassword;
--- infrastructures/symfony/Form/Type/UserType.php
+++ infrastructures/symfony/Form/Type/UserType.php
@@ -28,8 +28,6 @@ namespace Teknoo\East\WebsiteBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
